37f6185a5f1a8648e2036a2e3684fdaeaaa7c9a5
Emma10003 <5emma10003@gmail.com>
2025-12-18 18:22:55 +0900

    commonService api 반환 경로 변경 (getImageUrl)

--- src/service/commonService.js
+++ src/service/commonService.js
@@ -11,13 +11,12 @@ export const API_BASE_URL = 'https://3.37.46.8:9000';
  */
 
 export const getImageUrl = (path) => {
-    return `${API_BASE_URL}${path}`;
     if(!path) return '/static/img/default-avatar.jpg';
-    if(path.startsWith('http')) return path;
+    if(path.startsWith('http')) return `${API_BASE_URL}${path}`;
     if(path === 'default-avatar.jpg') return '/static/img/default-avatar.jpg';
     if(path === 'default-avatar.png') return '/static/img/default-avatar.jpg';
 
-    // return `${API_BASE_URL}${path}`;
+    return `${API_BASE_URL}${path}`;
 }
 
 // 날짜 포맷팅
